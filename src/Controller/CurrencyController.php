0e5e3b6a99d23f74eccf6748daaf8b4fe9c0b9d1
mosinskidamian11@gmail.com <mosinskidamian11@gmail.com>
2024-03-17 17:19:17 +0100

    feat: Dodanie Exception, Response tool oraz exceptions wraz z Subscriberem

--- src/Controller/CurrencyController.php
+++ src/Controller/CurrencyController.php
@@ -2,12 +2,26 @@
 
 namespace App\Controller;
 
+use App\Model\Exception\DataNotFoundModel;
+use App\Model\Exception\JsonDataInvalidModel;
+use App\Tool\ResponseTool;
+use Nelmio\ApiDocBundle\Annotation\Model;
 use OpenApi\Attributes as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[OA\Response(
+    response: 400,
+    description: "JSON Data Invalid",
+    content: new Model(type: JsonDataInvalidModel::class)
+)]
+#[OA\Response(
+    response: 404,
+    description: "Data not found",
+    content: new Model(type: DataNotFoundModel::class)
+)]
 #[OA\Tag(name: "Currency")]
 class CurrencyController extends AbstractController
 {
@@ -24,7 +38,7 @@ class CurrencyController extends AbstractController
     )]
     public function checkCurrency(): Response
     {
-        return new Response();
+        return ResponseTool::getResponse();
     }
 
     #[Route('/api/currency', name: 'exchangeRateCurrency', methods: ['POST'])]
@@ -40,6 +54,6 @@ class CurrencyController extends AbstractController
     )]
     public function exchangeRateCurrency(Request $request): Response
     {
-        return new Response();
+        return ResponseTool::getResponse();
     }
 }
---
+++ src/Model/Exception/DataNotFoundModel.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Model\Exception;
+
+use App\Model\ModelInterface;
+
+class DataNotFoundModel implements ModelInterface
+{
+    private string $error = "Data not found";
+
+    private array $data;
+
+    /**
+     * @param string[] $data
+     */
+    public function __construct(array $data = [])
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * @return string
+     */
+    public function getError(): string
+    {
+        return $this->error;
+    }
+
+    /**
+     * @param string $error
+     */
+    public function setError(string $error): void
+    {
+        $this->error = $error;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getData(): array
+    {
+        return $this->data;
+    }
+
+    /**
+     * @param string[] $data
+     */
+    public function setData(array $data): void
+    {
+        $this->data = $data;
+    }
+}
